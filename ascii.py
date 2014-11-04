d0768e9e8b64a715acfc60aefaa5bfd4c33607f5
Mack Ward <mackorone@gmail.com>
2014-11-04 14:44:20 -0500

    Centered the image

--- ascii.py
+++ ascii.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 # TODO: Use less CPU
+# TODO: CTRL-C???
 
 # Returns the number of rows and columns of characters
 def get_winsize():
@@ -18,31 +19,43 @@ def pixel_to_char(pixel):
 
 def main(arg):
 
-    # Initialize curses and turn off blinking curser
+    # Initialize curses
     curses.initscr()
     curses.curs_set(0)
+    curses.use_default_colors()
 
+    # Create the window
     rows, columns = get_winsize()
     win = curses.newwin(rows, columns, 0, 0)
 
+    # Initialize the capture device
     cap = cv2.VideoCapture(0)
     win.refresh()
 
+    # Get the initial compression_factor
     ret, img = cap.read()
     compression_factor = max(len(img)/rows, len(img[0])/columns)
 
+    # Function for drawing the image as characters
     def draw(img):
-        for row in range(len(img)/compression_factor):
-            for col in range(len(img[0])/compression_factor):
+        output_rows = len(img)/compression_factor
+        output_cols = len(img[0])/compression_factor
+        padding_rows = (rows - output_rows) / 2
+        padding_cols = (columns - output_cols) / 2
+        for row in range(output_rows):
+            for col in range(output_cols):
                 if curses.is_term_resized(rows, columns):
                     return
                 try:
                     pixel = img[row*compression_factor][col*compression_factor]
                     char = pixel_to_char(pixel)
-                    win.addch(row, col, char)
+                    win.addch(row + padding_rows, col + padding_cols, char)
                 except:
+                    # win.addch() causes an exception if we try to draw beyond
+                    # the boundaries of the window. We can just ignore it.
                     pass
         
+    # Loop forever, handling terminal resizes
     while True:
         ret, img = cap.read()
         draw(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
@@ -52,4 +65,5 @@ def main(arg):
             compression_factor = max(len(img)/rows, len(img[0])/columns)
         win.refresh()
 
+# Ensure curses is cleaned up correctly
 curses.wrapper(main)
