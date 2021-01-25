3c552d3e6e23af640041214c5d3b9c53674aa881
Mack Ward <mackorone@gmail.com>
2021-01-25 03:02:49 -0500

    Add quick start, format with black

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-ascii
-=====
+# ascii
 
 Render webcam video in the terminal, built with [opencv-python](https://pypi.org/project/opencv-python/)
+
+## Quick start
+
+```
+$ python3.8 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+$ python ascii.py
+```
 
 ![](https://github.com/mackorone/ascii/blob/master/demo.gif)
--- ascii.py
+++ ascii.py
@@ -1,17 +1,22 @@
+#!/usr/bin/env python3
+
 import curses
 import cv2
 import os
 import sys
 
+
 def get_winsize():
-    rows, columns = os.popen('stty size', 'r').read().split()
+    rows, columns = os.popen("stty size", "r").read().split()
     return int(rows), int(columns)
 
+
 def pixel_to_char(pixel):
-    ramp = '  .:-=+*#%@'
-    brightness = pixel/255.0
-    ramp_index = int(len(ramp)*brightness)
+    ramp = "  .:-=+*#%@"
+    brightness = pixel / 255.0
+    ramp_index = int(len(ramp) * brightness)
     return ramp[ramp_index]
+
 
 def main(arg):
 
@@ -30,14 +35,14 @@ def main(arg):
 
     # Get the initial compression_factor
     ret, img = cap.read()
-    compression_factor = max(len(img)/rows, len(img[0])/columns)
+    compression_factor = max(len(img) // rows, len(img[0]) // columns)
 
     # Function for drawing the image as characters
     def draw(img):
-        output_rows = len(img)/compression_factor
-        output_cols = len(img[0])/compression_factor * 2
-        padding_rows = (rows - output_rows) / 2
-        padding_cols = (columns - output_cols) / 2
+        output_rows = len(img) // compression_factor
+        output_cols = len(img[0]) // compression_factor * 2
+        padding_rows = (rows - output_rows) // 2
+        padding_cols = (columns - output_cols) // 2
         for row in range(output_rows):
             for col in range(output_cols):
                 if curses.is_term_resized(rows, columns):
@@ -50,16 +55,17 @@ def main(arg):
                     # win.addch() causes an exception if we try to draw beyond
                     # the boundaries of the window. We can just ignore it.
                     pass
-        
+
     # Loop forever, handling terminal resizes
     while True:
         ret, img = cap.read()
         draw(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
         if curses.is_term_resized(rows, columns):
-            rows, columns = get_winsize() 
+            rows, columns = get_winsize()
             win = curses.newwin(rows, columns, 0, 0)
-            compression_factor = max(len(img)/rows, len(img[0])/columns)
+            compression_factor = max(len(img) // rows, len(img[0]) // columns)
         win.refresh()
+
 
 # Ensure curses is cleaned up correctly
 curses.wrapper(main)
